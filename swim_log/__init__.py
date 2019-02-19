d2b6ef37438df2fb6d69d6c3516cb3c9211fa4bd
David Salazkin <david.salazkin@gmail.com>
2019-02-18 18:58:05 -0600

    adding user registration and associated error handling

--- swim_log/__init__.py
+++ swim_log/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cbc1768c680c69abbda0b52212d01008'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from swim_log import routes
