f9929b4c339e9def6e398233e92336e63d3a97f7
David Salazkin <david.salazkin@gmail.com>
2019-04-07 11:42:24 -0500

    Fixed active links

--- swim_log/main/routes.py
+++ swim_log/main/routes.py
@@ -9,7 +9,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template('home.html', legend='home')
+    return render_template('home.html', title='Home', legend='home')
 
 
 @main.route("/dashboard")
@@ -25,5 +25,5 @@ def history(username):
         abort(403)
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
-    sessions = Session.query.filter_by(swimmer=user).order_by(Session.session_date.desc()).paginate(page=page, per_page=5)
+    sessions = Session.query.filter_by(swimmer=user).order_by(Session.session_date.desc()).paginate(page=page, per_page=10)
     return render_template('history.html', title='History', sessions=sessions, user=user)
